d46f5a108c4c312689413f70c2f2d3f39f14b030
Yiwei Gao <ygao93@gmail.com>
2015-04-30 16:22:09 -0400

    settings activity can now be displayed from menu bar

---
+++ app/src/main/java/edu/emory/esacheckin/SettingsActivity.java
@@ -0,0 +1,16 @@
+package edu.emory.esacheckin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yiweigao on 4/30/15.
+ */
+
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
