c550317b313dbfc3e714393d0515023dd47da6ab
Yiwei Gao <ygao93@gmail.com>
2015-04-30 22:32:35 -0400

    added up navigation to settings activity

--- app/src/main/java/edu/emory/esacheckin/MainActivity.java
+++ app/src/main/java/edu/emory/esacheckin/MainActivity.java
@@ -2,17 +2,19 @@ package edu.emory.esacheckin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
     }
 
 
--- app/src/main/java/edu/emory/esacheckin/SettingsActivity.java
+++ app/src/main/java/edu/emory/esacheckin/SettingsActivity.java
@@ -1,17 +1,19 @@
 package edu.emory.esacheckin;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by yiweigao on 4/30/15.
  */
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
     }
 }
